81bb83991a07eaab08b605b141b5e8bb9afffeb5
sandorszucs <sandor_szucs_lma@yahoo.com>
2018-09-14 17:39:54 +0300

    Other Exercises

---
+++ src/org/fasttrackit/algorithm/sandorszucs/HomeWork/Exercise10.java
@@ -0,0 +1,53 @@
+package org.fasttrackit.algorithm.sandorszucs.HomeWork;
+import java.util.Scanner;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        int v = 0;
+        int[] a = new int[30];
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("What number would you like to search?");
+        int x = sc.nextInt();
+
+        int index;
+        for(index = 0; index < n; ++index) {
+            System.out.println("Enter the elements of the string");
+            a[index] = sc.nextInt();
+        }
+
+        while(flag) {
+            flag = false;
+
+            for(int j = 0; j < n - 1; ++j) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        System.out.println("The organized string is: ");
+
+        for(index = 0; index < n; ++index) {
+            System.out.print(" a =  " + a[index] + "  ");
+        }
+        System.out.print("\n");
+
+        for(index = 0; index < n; ++index) {
+            if (a[index] == x) {
+                System.out.println("The number you find at " + index + " position");
+                break;
+            }
+            ++v;
+        }
+
+        if (v == n) {
+            System.out.println("-1");
+        }
+    }
+}
---
+++ src/org/fasttrackit/algorithm/sandorszucs/HomeWork/Exercise8.java
@@ -0,0 +1,24 @@
+package org.fasttrackit.algorithm.sandorszucs.HomeWork;
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        System.out.println("Check whether your number is Polindrom or Not");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int userEnteredNumber = number;
+
+        int remainder;
+        int temp;
+        for(temp = 0; number > 0; temp = temp * 10 + remainder) {
+            remainder = number % 10;
+            number /= 10;
+        }
+
+        if (userEnteredNumber == temp) {
+            System.out.println("Your number IS Polindrome");
+        } else {
+            System.out.println("Your number IS NOT Polindrome");
+        }
+    }
+}
