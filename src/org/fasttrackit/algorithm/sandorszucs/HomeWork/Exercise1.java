ca711b29731e673f334718cfd0186e218c48b0e1
sandorszucs <sandor_szucs_lma@yahoo.com>
2018-09-14 16:50:37 +0300

    First exercise

---
+++ src/org/fasttrackit/algorithm/sandorszucs/HomeWork/Exercise1.java
@@ -0,0 +1,18 @@
+package org.fasttrackit.algorithm.sandorszucs.HomeWork;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the first number: ");
+
+            double number1 = input.nextDouble();
+            System.out.println("Enter the second number: ");
+
+            double number2 = input.nextDouble();
+            double sumOfNumbers = (number1 + number2) / 2;
+
+            System.out.println("The arithmetical value of the numbers is: " + sumOfNumbers);
+        }
+    }
