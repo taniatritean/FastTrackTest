4555bcdc43d400f67e6cfd061012008f9fa1d4c5
sandorszucs <sandor_szucs_lma@yahoo.com>
2018-09-14 16:59:44 +0300

    Moved Files

---
+++ src/org/fasttrackit/algorithm/sandorszucs/HomeWork/Exercise2.java
@@ -0,0 +1,22 @@
+package org.fasttrackit.algorithm.sandorszucs.HomeWork;
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers would you like to insert?");
+        int length = sc.nextInt();
+        double sum = 0.0D;
+        int[] numbers = new int[length];
+
+        for(int i = 0; i < numbers.length; ++i) {
+            System.out.println("Enter the next number: ");
+            numbers[i] = sc.nextInt();
+            sum += (double)numbers[i];
+        }
+
+        double mean = sum / (double)numbers.length;
+        System.out.println("Sum of the number is " + sum);
+        System.out.println("Mean of the number is " + mean);
+    }
+}
---
+++ src/org/fasttrackit/algorithm/sandorszucs/HomeWork/Exercise5.java
@@ -0,0 +1,23 @@
+package org.fasttrackit.algorithm.sandorszucs.HomeWork;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println("Enter the first wished number: ");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second wished number: ");
+        int secondNumber = scanner.nextInt();
+
+        if (firstNumber > secondNumber) {
+            int temp = secondNumber;
+            secondNumber = firstNumber;
+            firstNumber = temp;
+        }
+
+        for(int i = firstNumber; i <= secondNumber; ++i) {
+            System.out.println(firstNumber + "x" + i + " = " + firstNumber * i + "           |             " + secondNumber + "x" + i + "  = " + secondNumber * i);
+        }
+    }
+}
