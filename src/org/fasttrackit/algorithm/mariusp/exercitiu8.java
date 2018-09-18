c5b4ef412f9faca5a258804b0e47a745395483e7
mariusp093 <42910757+mariusp093@users.noreply.github.com>
2018-09-18 19:02:50 +0300

    marius p

--- src/org/fasttrackit/algorithm/mariusp/exercitiu8.java
+++ src/org/fasttrackit/algorithm/mariusp/exercitiu8.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class exercitiu8 {
     public static void main(String[]args){
-
+//test
 
         Scanner input=new Scanner(System.in);
         System.out.println("Numarul este:");
