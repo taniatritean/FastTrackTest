3fbd73ebcf076131f1a9ff2014ac8473ff930e66
manhorro <manhorro@yahoo.com>
2018-09-18 19:40:30 +0300

    marius p

---
+++ src/org/fasttrackit/algorithm/horatiu/test.java
@@ -0,0 +1,12 @@
+package org.fasttrackit.algorithm.horatiu;
+
+
+public class MedieAritmeticaHoratiu {
+    public static void main (String[] args){
+        int firstnumber = 200;
+        int secondnumber = 123;
+        int sum;
+        sum = firstnumber + secondnumber;
+        System.out.println("Suma numerelor este:" + sum);
+    }
+}
