6d7c89aeb2d0db1731a2c8825d19720fddf0a9c8
Julya <iuliadiana27@gmail.com>
2018-09-18 20:56:28 +0300

    Exercitiu1

--- src/org/fasttrackit/algorithm/girdavasile/MediaAritmeticaSir.java
+++ src/org/fasttrackit/algorithm/girdavasile/MediaAritmeticaSir.java
@@ -1,25 +1,34 @@
 package org.fasttrackit.algorithm.girdavasile;
 
 public class MediaAritmeticaSir {
+    public static void main(String args[]) {
 
-
-    public static void main(String[] args){
-
-        int FirstNumber = 201;
+        int firstNumber = 201;
         int secondNumber = 123;
-
         int sum;
 
-        sum = FirstNumber + secondNumber;
-        System.out.println("suma numerelor este " + sum);
-    }
-    String firstNumberstring = "" + FirstNumber;
+        sum = firstNumber + secondNumber;
+        System.out.println("Suma numerelor este: " + sum);
 
-    firstnumberstring = Integer.toString(FirstNumber);
+        String firstNumberString = "" + firstNumber;
 
-    int sumFirstNumber = 0
-            for(int index=0;index<firstNumberString.length();index++)  {
-                sumFirstNumber+=Integer.parseIn
+        firstNumberString = Integer.toString(firstNumber);
+        String secondNumberString = Integer.toString(secondNumber);
+
+        int sumFirstNumber = 0;
+
+        for (int index = 0; index <firstNumberString.length(); index++) {
+            sumFirstNumber += Integer.parseInt(""+ firstNumberString.charAt(index));
+        }
+
+        int sumSecondNumber = 0;
+        for (int index = 0; index <secondNumberString.length(); index++) {
+            sumSecondNumber += Integer.parseInt(""+ secondNumberString.charAt(index));
+        }
+        System.out.println("Sum first number:  " + sumFirstNumber);
+        System.out.println("Sum second number: " + sumSecondNumber);
     }
 }
+
+
 
