c0e93ab906d30ac083ce2b85ca16350bf2086b25
sandorszucs <sandor_szucs_lma@yahoo.com>
2018-09-23 14:06:16 +0300

    Exercise solved

---
+++ src/org/fasttrackit/algorithm/sandorszucs/homework/Exercise16.java
@@ -0,0 +1,26 @@
+package src.org.fasttrackit.algorithm.sandorszucs.homework;
+
+import java.util.Scanner;
+
+public class Exercise16 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many numbers would you like to enter: ");
+        int enteredNumber = scanner.nextInt();
+
+        int[] array = new int[enteredNumber];
+        for (int i = 0; i < enteredNumber; i++) {
+            System.out.println("Insert numbers in the array:");
+            array[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("The requested numbers are: ");
+
+        for (int i = 1; i < enteredNumber; i++) {
+            if (array[i - 1] > array[i]) {
+                System.out.println(array[i - 1] + " " + array[i] + " - ");
+            }
+        }
+    }
+}
