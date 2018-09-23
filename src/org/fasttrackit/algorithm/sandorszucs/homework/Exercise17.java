016cd155c4c63bcde1a3dc2b3274b8367e62f4dc
sandorszucs <sandor_szucs_lma@yahoo.com>
2018-09-23 14:37:04 +0300

    Exercise solved

---
+++ src/org/fasttrackit/algorithm/sandorszucs/homework/Exercise17.java
@@ -0,0 +1,17 @@
+package src.org.fasttrackit.algorithm.sandorszucs.homework;
+
+import java.util.Scanner;
+
+public class Exercise17 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number and the square: ");
+        int x = scan.nextInt();
+        int n = scan.nextInt();
+        double sum = 0;
+        for(int i=0; i<=n; i++){
+            sum = sum + Math.pow(x, i);
+        }
+        System.out.println("Total is: " + sum);
+    }
+}
