b05b317a9d13092d92b8eb1426dbce4eab424cb4
manhorro <manhorro@yahoo.com>
2018-10-15 12:22:50 +0300

    Problema lab3

---
+++ src/org/fasttrackit/algorithm/horatiu/laborator7/Magazin.java
@@ -0,0 +1,17 @@
+package org.fasttrackit.algorithm.horatiu.laborator7;
+//    Se citeste de la tastatura sau dintr-un fisier (optional puteti invata citirea din fisiere)
+// o lista de obiecte Produs din cadrul unui magazin. Produsul are nume, categorie si pret.
+//    Doua produse sunt considerate egale daca au acelasi nume.
+//    -	Daca utilizatorul a introdus un duplicat, va rog aruncati o exceptie custom scrisa de voi:
+//    DuplicatedProduct.
+//    -	Sa se afiseze pe ecran toate elementele citite, ordonate alphabetic dupa nume.
+//    -	Sa se afiseze toate produsele citite, grupate pe categorie (sa aveti cel putin 2 produse in
+// fiecare categorie, minim 3 categorii)
+//    -	Sa se afiseze numarul produselor din fiecare categorie(cate elemente avem in fiecare category ex:
+// categorie1: 4 produse, categorie2: 3 produse)
+//    -	Sa se afiseze din fiecare categorie produsul cel mai scump (folosind metoda toString din clasa Podus-
+// sa se returneze “produsul ” + nume+ “ din categoria ” +categorie+” are pretul ” +pret)
+
+
+public class Magazin {
+}
