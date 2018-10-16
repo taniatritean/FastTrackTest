0e856f4981e3c86d3a68c4098b0cc721b6a9e390
sandorszucs <szucs_sandor_lma@yahoo.com>
2018-10-16 20:41:22 +0300

    Exercise during the course - added to github

---
+++ src/org/fasttrackit/algorithm/sandorszucs/homework/lab8/ClasaDeLegatura.java
@@ -0,0 +1,8 @@
+package org.fasttrackit.algorithm.sandorszucs.homework.lab8;
+
+public class ClasaDeLegatura {
+
+    public static void printNumberOfPopulation() {
+        System.out.println(Om.nrOfPopulation);
+    }
+}
