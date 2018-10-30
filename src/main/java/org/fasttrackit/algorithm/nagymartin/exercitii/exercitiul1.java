2ba10fe5a78525394b3d99509067d022b440d3ec
NagyMartin <nagymartin1991@gmail.com>
2018-10-30 15:18:17 +0200

    primul exercitiu 1.1

---
+++ src/main/java/org/fasttrackit/algorithm/nagymartin/exercitii/exercitiul1.java
@@ -0,0 +1,21 @@
+package org.fasttrackit.algorithm.nagymartin.exercitii;
+
+public class exercitiul1 {
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 6;
+        int mediaAritmetica;
+
+        if (firstNumber <= 0) {
+            System.out.println("Numerele trebuie sa fie pozitive");
+        }
+        if (secondNumber <= 0) {
+            System.out.println("Numerele trebuie sa fie pozitive");
+        }
+
+        mediaAritmetica = (firstNumber + secondNumber) / 2;
+        System.out.println("Media aritmetica este " +  mediaAritmetica);
+
+
+    }
+}
---
+++ src/main/java/org/fasttrackit/algorithm/nagymartin/exercitii/exercitiul2.java
@@ -0,0 +1,8 @@
+package org.fasttrackit.algorithm.nagymartin.exercitii;
+
+public class exercitiul2 {
+    public static void main(String[] args) {
+
+    }
+
+}
